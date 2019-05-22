1145136837df0689d1ef3674cc188d264f089bd1
valentyn <melnychukv@cip.ifi.lmu.de>
2019-05-23 00:05:02 +0200

    Prediction, based on last current chat

--- src/telegram_bot/__init__.py
+++ src/telegram_bot/__init__.py
@@ -87,23 +87,6 @@ class TelegramBot:
         """Send a message when the command /help is issued."""
         update.message.reply_text('Help!')
 
-    def _get_emoji_list(self):
-        # emoji = getEmoji(True)
-        thunderstorm = u'\U0001F4A8'  # Code: 200's, 900, 901, 902, 905
-        drizzle = u'\U0001F4A7'  # Code: 300's
-        rain = u'\U00002614'  # Code: 500's
-        snowflake = u'\U00002744'  # Code: 600's snowflake
-        snowman = u'\U000026C4'  # Code: 600's snowman, 903, 906
-        atmosphere = u'\U0001F301'  # Code: 700's foogy
-        clearSky = u'\U00002600'  # Code: 800 clear sky
-        fewClouds = u'\U000026C5'  # Code: 801 sun behind clouds
-        clouds = u'\U00002601'  # Code: 802-803-804 clouds general
-        hot = u'\U0001F525'  # Code: 904
-        defaultEmoji = u'\U0001F300'
-
-        return np.random.permutation([thunderstorm, drizzle, rain, snowflake, snowman, atmosphere, clearSky,
-                                            fewClouds, clouds, hot, defaultEmoji])
-
     def _handle_message(self, update, context):
 
         #Adding new chat
@@ -116,6 +99,7 @@ class TelegramBot:
 
         current_chat.add_message(update.message)
         self.saver.save_one(update.message)
+        MEME_CHAT_ID = current_chat.chat_id
 
         # results = [[sticker.emoji for sticker in self.stiker_set]]
         # markup = ReplyKeyboardMarkup(results, one_time_keyboard=True, resize_keyboard=True, selective=True)
