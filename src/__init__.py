8aa8638e0a1bf9a2e4c70d5c894fd85aaed56933
valentyn <melnychukv@cip.ifi.lmu.de>
2019-07-01 14:59:12 +0200

    Replace new vocabulary, fix bugs in sentence_tokenizer

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,13 @@
+import os
 from os.path import abspath, dirname
 
 ROOT_PATH = dirname(dirname(abspath(__file__)))
 
+PRETRAINED_VOCABULARY_SIZE = 50000
+
 TRAIN_DATASET_PATH = f'{ROOT_PATH}/data/processed/appendix1_test.csv'
-VA_REGRESSION_WEIGHTS_PATH = f'{ROOT_PATH}/models/text2va/va_regression.pth'
+VA_REGRESSION_FOLDER = f'{ROOT_PATH}/models/text2va'
+VA_REGRESSION_WEIGHTS_PATH = f'{VA_REGRESSION_FOLDER}/va_regression.pth'
 VOCAB_PATH = f'{ROOT_PATH}/models/vocabulary.json'
+
+os.makedirs(VA_REGRESSION_FOLDER, exist_ok=True)
--- src/features/sentence_tokenizer.py
+++ src/features/sentence_tokenizer.py
@@ -25,7 +25,8 @@ class SentenceTokenizer:
         else:
             self.vocabulary = self.lemmatize_vocab(vocabulary)
 
-    def decontracted(phrase):
+
+    def decontracted(self, phrase):
         # specific
         phrase = re.sub(r"won't", "will not", phrase)
         phrase = re.sub(r"can\'t", "can not", phrase)
@@ -63,31 +64,33 @@ class SentenceTokenizer:
     def save_vocabulary(self, vocabulary):
 
         with open(VOCAB_PATH, 'w') as outfile:
-            json.dump(vocabulary, outfile)
+            json.dump(vocabulary, outfile, indent=2)
         outfile.close()
 
-    def tokenize_sentences(self, sentences, maxlen=30):
+    def tokenize_sentences(self, sentences, maxlen=50, save=False):
         """Converts a given list of sentences into a array of integers according to vocabulary"""
-        sentences = ['I love mom\'s cooking',
-                          'I love how you never reply back..',
-                          'I love cruising with my homies',
-                          'I love messing with yo mind!!',
-                          'I love you and now you\'re just gone..',
-                          'This is shit',
-                          'This is the shit']
+
         tokens_matr = np.zeros((len(sentences), maxlen), dtype='uint16')
-        arr_tokens = []
         i = 0
         for sentence in sentences:
             sentence = self.decontracted(sentence)
             # take only words
             sentence = re.sub('[' + string.punctuation + ']', '', sentence).split()
-            tokens = [self.vocabulary[k.lower()] if k.lower() in self.vocabulary.keys() else self.extend_vocabulary(k.lower()) for k in sentence]
+            tokens = [self.vocabulary[k.lower()] if k.lower() in self.vocabulary.keys() else self.vocabulary['CUSTOM_UNKNOWN'] for k in sentence]
+
+            # Empty sentence
+            if len(tokens) == 0:
+                tokens = [self.vocabulary['CUSTOM_UNKNOWN']]
+
             for j in range(len(tokens)):
-                if j<maxlen:
+                if j < maxlen:
                     tokens_matr[i, j] = tokens[j]
             i += 1
-        self.save_vocabulary(self.vocabulary)
+
+
+
+        if save:
+            self.save_vocabulary(self.vocabulary)
         return tokens_matr
 
     def to_sentence(self, arr_tokens):
